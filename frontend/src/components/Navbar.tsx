9cc0d900e85d437f214be6220963976e0bfb24b3
Michael Caridad <caridadmichael@gmail.com>
2022-12-27 00:56:09 -0400

    styling.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
 
 function NavBar() {
   return (
-   <> <nav><a href="#">Abercrombie & Fitch</a></nav>
-   <div>
-    <ul><li><a href="#">Men's</a></li></ul>
-    <ul><li><a href="#">Women's</a></li></ul>
-    <ul><li><a href="#">Kid's</a></li></ul>
-   </div></>
-  )
+    <>
+      <header className="absolute top-0 left-0 w-full h-20 py-5 px-10 flex justify-between items-center bg-white">
+        <Link to="#" className="text-[#253746]">
+          Brand
+        </Link>
+        <nav>
+          <div className="group">
+            <ul className="navigation">
+              <li>
+                <Link to="#">Men's</Link>
+              </li>
+              <li>
+                <Link to="#">Women's</Link>
+              </li>
+              <li>
+                <Link to="#">Kids</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="search">
+            <span className="icon">
+              <CiSearch />
+              <AiOutlineClose />
+            </span>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
-
 export default NavBar;
-
