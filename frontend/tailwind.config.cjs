d9d39f3b4270604b7ef1cdea31862d94cce6d598
Michael Caridad <caridadmichael@gmail.com>
2022-12-28 19:01:49 -0400

    add animation

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,7 +5,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing:{
+        letterSpace : '30px'
+      } ,
+      keyframes:{
+        'open-menu':{
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.2)'},
+          '100%':{transform : 'scale(1)'},
+        },
+      },
+      animation:{
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
   },
   plugins: [],
 }
