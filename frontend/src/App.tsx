864ee75db95b7760b4fa2f83c9ff04fb9dffe51d
Michael Caridad <caridadmichael@gmail.com>
2023-02-08 19:05:09 -0400

    add new fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import Navbar from "./components/Navbar";
 import MainPage from "./pages/MainPage";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import ProductPage from "./pages/ProductPage";
+import CheckOutPage from "./pages/CheckOutPage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <MainPage />
+      <CheckOutPage />
+      {/* <MainPage /> */}
+      {/* <ProductPage /> */}
     </>
   );
 }
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        "Cor" : ['Cormorant Garamond','Arial']
+      },
+      
       letterSpacing:{
         letterSpace : '30px'
       } ,
